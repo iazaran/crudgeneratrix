7b93f123026837e328f608b6ef8f00f558b59329
Ismael Azaran <eazaran@gmail.com>
2023-01-03 01:13:43 +0400

    Callbacks added to the CRUD samples

--- samples.php
+++ samples.php
@@ -3,16 +3,52 @@
 require_once 'src/GeneratrixDB.php';
 require_once 'src/GeneratrixCRUD.php';
 
-$generatrixDB = new \iazaran\crudgeneratrix\GeneratrixDB('locations', 'DBUser', 'DBPassword', 'localhost');
-$generatrixAPI = new \iazaran\crudgeneratrix\GeneratrixCRUD($generatrixDB, 'JSON');
+class CustomMethods
+{
+    /**
+     * Sample class and method to use as callback
+     *
+     * @param ...$responses
+     * @return array
+     */
+    public static function groupByFirstColumn(...$responses): array
+    {
+        $tempResponse = [];
+        foreach ($responses as $response) {
+            $firstColumn = array_key_first($response);
+            array_shift($response);
+            $tempResponse[$firstColumn][] = $response;
+        }
+
+        return $tempResponse;
+    }
+}
+
+// Creating instances and specify the result type like JSON or RAW array of data
+$generatrixDB = new \iazaran\crudgeneratrix\GeneratrixDB('locations', 'DBUser', 'DBPassword995!', 'localhost');
+$generatrixCRUD = new \iazaran\crudgeneratrix\GeneratrixCRUD($generatrixDB, 'JSON');
 
 try {
-    var_dump($generatrixAPI::information(['tt_countries' => ['countryCode', 'countryName']]));
-    var_dump($generatrixAPI::read(30, ['tt_cities' => ['cityName']], ['tt_hotels' => ['name']], 'LEFT'));
-    var_dump($generatrixAPI::read(30, ['tt_hotels' => ['name']], ['tt_cities' => ['cityName']], 'RIGHT'));
-    var_dump($generatrixAPI::create(['tt_countries' => ['countryCode' => ['US', 'GB'], 'countryName' => ['United State', 'Great Britain']]]));
-    var_dump($generatrixAPI::update(237, ['tt_countries' => ['countryCode' => 'ES', 'countryName' => 'Spain']]));
-    var_dump($generatrixAPI::delete(237, ['tt_countries']));
+    // Getting information about a table and some columns
+    var_dump($generatrixCRUD::information(['tt_countries' => ['countryCode', 'countryName']]));
+
+    // Reading a specific row from a table and related table(s) and columns based on different relationship directions
+    var_dump($generatrixCRUD::read(30, ['tt_cities' => ['cityName']], ['tt_hotels' => ['name']], 'LEFT'));
+    var_dump($generatrixCRUD::read(30, ['tt_hotels' => ['name']], ['tt_cities' => ['cityName']], 'RIGHT'));
+
+    // Using custom method as callback
+    // Without callback like: [{"cityName":"Al Ain","name":"Radisson Blu Hotel & Resort, Al Ain"},{"cityName":"Al Ain","name":"Danat Al Ain Resort"},{"cityName":"Al Ain","name":"Mercure Grand Jebel Hafeet Al Ain Hotel"}]
+    // With callback like: {"cityName":[{"name":"Radisson Blu Hotel & Resort, Al Ain"},{"name":"Danat Al Ain Resort"},{"name":"Mercure Grand Jebel Hafeet Al Ain Hotel"}]}
+    var_dump($generatrixCRUD::read(30, ['tt_cities' => ['cityName']], ['tt_hotels' => ['name']], 'LEFT', ['CustomMethods', 'groupByFirstColumn']));
+
+    // Create multiple rows
+    var_dump($generatrixCRUD::create(['tt_countries' => ['countryCode' => ['US', 'GB'], 'countryName' => ['United State', 'Great Britain']]]));
+
+    // Update specific row
+    var_dump($generatrixCRUD::update(237, ['tt_countries' => ['countryCode' => 'ES', 'countryName' => 'Spain']]));
+
+    // Delete specific row
+    var_dump($generatrixCRUD::delete(237, ['tt_countries']));
 } catch (Exception $e) {
     var_dump($e->getMessage());
 }
